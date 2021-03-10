1bee2d78ec1ae45cbe74ec2fc3cf135cf3a13805
enida <enida_mujezin_16@yahoo.com>
2021-03-10 22:42:18 +0100

    Adding comments in JS

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+
+    //This is a single line JavaScript comment
+    /*This is a multi-line comment
+    This is a multi-line comment
+    */
+     
+    
+     }
+    console.log(main());
+    module.exports = main;
