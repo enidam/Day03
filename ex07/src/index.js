454a4c18116018eaa07259c4f8a085fe6fd5a4bf
enida <enida_mujezin_16@yahoo.com>
2021-03-10 23:48:49 +0100

    camelCase

---
+++ ex07/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseCover;
+
+    studlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseCover = 9000;
+        return {
+            studlyCapVar,
+            properCamelCase,
+            titleCaseCover
+}
+         }
+        console.log(main());
+        module.exports = main;
